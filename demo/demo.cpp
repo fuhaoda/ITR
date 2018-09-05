bf9bed7ece2eb88f9e2e154e9944ed03213c4584
Bo Zhang <zhang_bo3@lilly.com>
2018-09-05 12:09:23 -0400

    clean up demo code and minor update to top level CMakeLists.txt

--- demo/demo.cpp
+++ demo/demo.cpp
@@ -1,72 +1,22 @@
-#include <cstdio>
-#include <getopt.h>
 #include <string>
 #include <sys/stat.h> 
 #include "ITR.h"
 
-void print_usage(char *program) {
-  fprintf(stdout, "Usage: %s --input=FILE --depth=NUM\n", program);
-}
-
-bool parse_cmd_args(int argc, char **argv,
-                    std::string &input, unsigned &depth) {
-  static struct option long_options[] =
-    {
-     {"input", required_argument, 0, 'i'},
-     {"depth", required_argument, 0, 'd'},
-     {"help", no_argument, 0, 'h'},
-     {0, 0, 0, 0}
-    };
-
-  int opt = 0;
-  int long_index = 0;
-  while ((opt = getopt_long(argc, argv, "i:d:h",
-                            long_options, &long_index)) != -1) {
-    switch (opt) {
-    case 'i':
-      input = optarg;
-      break;
-    case 'd':
-      depth = atoi(optarg);
-      break;
-    case 'h':
-      print_usage(argv[0]);
-      return false;
-    }
-  }
-
-  struct stat buffer;
-  if (stat(input.c_str(), &buffer) != 0) {
-    fprintf(stderr, "Usage ERROR: input file does not exist\n");
-    return false;
-  }
-
-  
-  if (depth != 1 && depth != 2 && depth != 3) {
-    fprintf(stderr, "Usage ERROR: depth of the search can only be 1, 2, or 3\n");
-    return false;
-  }
-  
-  return true; 
-}
 
 int main(int argc, char **argv) {
-  // Default values 
-  std::string input{"TestSample_100.csv"};
-  unsigned depth = 3; 
+  // Create an ITR instance. The constructor takes two arguments: the first one
+  // is the path of the input data file, and the second one is the depth of the
+  // search. 
+  ITR::ITR instance("TestSample_100.csv", 3); 
 
-  // Parse command line arguments if exist 
-  if (!parse_cmd_args(argc, argv, input, depth))
-    return -1;
-
-  // Create the ITR instance
-  ITR::ITR instance(input, depth); 
-
-  // Run the comprehensive search
+  // Run the search with the specified depth
   instance.run();
 
-  // Print out the report
-  instance.report(); 
+  // Display the result. By default, the function prints out the best value and
+  // the associated cut information. Optionally, one can provide an integer
+  // input, say n, and the function will print out information associated with
+  // the best n search results. 
+  instance.report(10); 
 
   return 0; 
 }
