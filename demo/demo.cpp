695c991bffd7daeb9f8ebcd10065737e5ecae335
Bo Zhang <zhang_bo3@lilly.com>
2018-09-07 14:59:19 -0400

    minor modify of demo file

--- demo/demo.cpp
+++ demo/demo.cpp
@@ -1,22 +1,31 @@
 #include <string>
-#include <sys/stat.h> 
+#include <iostream>
 #include "ITR.h"
 
-
 int main(int argc, char **argv) {
-  // Create an ITR instance. The constructor takes two arguments: the first one
-  // is the path of the input data file, and the second one is the depth of the
-  // search. 
-  ITR::ITR instance("demo_input.csv", 3); 
+  try {
+    // Create an ITR instance. The constructor takes two arguments: The first
+    // one is the path of the input data file, and the second one is the depth
+    // of the search.
 
-  // Run the search with the specified depth
-  instance.run();
+    // The valid values for the search depth are 1, 2, or 3.
 
-  // Display the result. By default, the function prints out the best value and
-  // the associated cut information. Optionally, one can provide an integer
-  // input, say n, and the function will print out information associated with
-  // the best n search results. 
-  instance.report(10); 
+    // The constructor will throw an exception if the file does not exist or if
+    // the depth of the search is invalid. 
+    ITR::ITR instance{"sample100.csv", 3};
+
+    // Run the search with specified depth
+    instance.run();
+
+    // Display the result. By default, the function prints out the best value
+    // and the associated cut information. Optionally, one can provide an
+    // integer input, say n, and the function will print out information
+    // associated with the best n search results. If n is larger than the number
+    // of searches performed, it will be truncated internally. 
+    instance.report(10);
+  } catch (const char *msg) {
+    std::cout << msg << "\n";
+  }
 
   return 0; 
 }
