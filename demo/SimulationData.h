6fe03dbe96fdb78da04d20cc3fbc00f8f28d3dac
Bo Zhang <zhang_bo3@lilly.com>
2018-09-12 10:34:13 -0400

    minor update to the data generation utility functions

--- demo/SimulationData.h
+++ demo/SimulationData.h
@@ -8,22 +8,37 @@ class SimulationData {
 public:
   // Constructor 
   SimulationData(size_t nSample, size_t nCont, size_t nOrd, size_t nNom,
-                 size_t nAct = 1, size_t nResp = 1); 
+                 size_t nResp = 1); 
 
   // Generate continuous variable. 
-  void generateContVariable(void(*func)(double *arr, size_t n));
+  void generateContVariable(void(*func)(double *, size_t, double, double),
+                            double a, double b) {
+    generateData<double>(cont_.get(), nCont_, func, a, b);
+  }
 
   // Generate ordinal variable. 
-  void generateOrdVariable(void(*func)(int *arr, size_t n));
+  void generateOrdVariable(void(*func)(int *, size_t, int, int),
+                           int a, int b) {
+    generateData<int>(ord_.get(), nOrd_, func, a, b);
+  }
 
   // Generate nominal variable. 
-  void generateNomVariable(void(*func)(int *arr, size_t n)); 
+  void generateNomVariable(void(*func)(int *, size_t, int, int),
+                           int a, int b) {
+    generateData<int>(nom_.get(), nNom_, func, a, b);
+  }
 
   // Generate actions. 
-  void generateAct(void(*func)(int *arr, size_t n));
+  void generateAct(void(*func)(int *, size_t, int, int),
+                   int a, int b) {
+    generateData<int>(act_.get(), 1, func, a, b);
+  }
 
   // Generate responses.
-  void generateResp(void(*func)(double *arr, size_t n)); 
+  void generateResp(void(*func)(double *, size_t, double, double),
+                    double a, double b) {
+    generateData<double>(resp_.get(), nResp_, func, a, b);
+  }
   
   // Save the generated data into a csv file
   void save(std::string fname = std::string{"simData.csv"}) const;   
@@ -33,7 +48,6 @@ private:
   size_t nCont_;   // Number of continuous variables to generate
   size_t nOrd_;    // Number of ordinal variables to generate
   size_t nNom_;    // Number of nominal variables to generate
-  size_t nAct_;    // Number of different actions
   size_t nResp_;   // Number of different responses
 
   std::unique_ptr<double []> cont_; // Buffer for continuous variable 
@@ -42,10 +56,11 @@ private:
   std::unique_ptr<int []> act_;     // Buffer for action
   std::unique_ptr<double []> resp_; // Buffer for response
 
-  template<typename T> void generateData(T *matrix, size_t nCol,
-                                         void(*func)(T *arr, size_t n)) {
+  template<typename T>
+  void generateData(T *matrix, size_t nCol,
+                    void(*func)(T *, size_t, T, T), T a, T b) {
     for (size_t i = 0; i < nCol; ++i) 
-      func(matrix + i * nSample_, nSample_); 
+      func(matrix + i * nSample_, nSample_, a, b); 
   }  
 };
 
--- demo/genData.cpp
+++ demo/genData.cpp
@@ -9,21 +9,24 @@ size_t nSample = 10;
 size_t nCont = 1;
 size_t nOrd = 1;
 size_t nNom = 1;
-size_t nAct = 1;
 size_t nResp = 1;
+double lDble = 0;
+double uDble = 1;
+int lInt = 0;
+int uInt = 5; 
 
 // By default, double values are drawn uniformly from [0, 1], and integer values
 // are also drawn uniformly from [0, 1]. 
 std::default_random_engine engine;
 
-void genDouble(double *arr, size_t n) {
-  std::uniform_real_distribution<> dist{0.0, 1.0};
+void genDouble(double *arr, size_t n, double a, double b) {
+  std::uniform_real_distribution<> dist{a, b};
   for (size_t i = 0; i < n; ++i)
     arr[i] = dist(engine);
 }
 
-void genInt(int *arr, size_t n) {
-  std::uniform_int_distribution<> dist{0, 5};
+void genInt(int *arr, size_t n, int a, int b) {
+  std::uniform_int_distribution<> dist{a, b};
   for (size_t i = 0; i < n; ++i)
     arr[i] = dist(engine);
 }
@@ -35,11 +38,13 @@ void printUsage(char *program) {
           "--nCont=NUM   Number of continuous variables, default is 1\n"
           "--nOrd=NUM    Number of ordinal variables, default is 1\n"
           "--nNom=NUM    Number of noninal variables, default is 1\n"
-          "--nAct=NUM    Number of actions, default is 1\n"
-          "--nResp=NUM   Number of responses, default is 1\n",
+          "--nResp=NUM   Number of responses, default is 1\n"
+          "--lDble=NUM   Lower bound of the continuous variable, default is 0\n"
+          "--uDble=Num   Upper bound of the continuous variable, default is 1\n"
+          "--lInt=NUM    Lower bound of the ord/nom variable, default is 0\n"
+          "--uInt=NUM    Upper bound of the ord/nom variable, default is 5\n",
           program);
 }
-
 
 int main(int argc, char **argv) {
   // Parse command line arguments
@@ -50,15 +55,18 @@ int main(int argc, char **argv) {
      {"nCont", required_argument, 0, 'c'},
      {"nOrd", required_argument, 0, 'o'},
      {"nNom", required_argument, 0, 'n'},
-     {"nAct", required_argument, 0, 'a'},
      {"nResp", required_argument, 0, 'r'},
+     {"lDble", required_argument, 0, 'a'},
+     {"uDble", required_argument, 0, 'b'},
+     {"lInt", required_argument, 0, 'A'},
+     {"uInt", required_argument, 0, 'B'}, 
      {"help", no_argument, 0, 'h'}, 
      {0, 0, 0, 0}
     }; 
 
   int opt = 0;
   int long_index = 0;
-  while ((opt = getopt_long(argc, argv, "s:p:c:o:n:a:r:h",
+  while ((opt = getopt_long(argc, argv, "s:p:c:o:n:r:a:b:A:B:h",
                             long_options, &long_index)) != -1) {
     switch(opt) {
     case 's':
@@ -76,12 +84,21 @@ int main(int argc, char **argv) {
     case 'n':
       nNom = atoi(optarg);
       break;
-    case 'a':
-      nAct = atoi(optarg);
-      break;
     case 'r':
       nResp = atoi(optarg);
       break;
+    case 'a':
+      lDble = atof(optarg);
+      break;
+    case 'b':
+      uDble = atof(optarg);
+      break;
+    case 'A':
+      lInt = atoi(optarg);
+      break;
+    case 'B':
+      uInt = atoi(optarg);
+      break;      
     case 'h':
     case '?': 
       printUsage(argv[0]);
@@ -93,14 +110,14 @@ int main(int argc, char **argv) {
   engine = std::default_random_engine{seed}; 
 
   // Create an instance of the SimulationData class
-  SimulationData data{nSample, nCont, nOrd, nNom, nAct, nResp};
+  SimulationData data{nSample, nCont, nOrd, nNom, nResp};
 
   // Generate actual data
-  data.generateContVariable(genDouble);
-  data.generateOrdVariable(genInt);
-  data.generateNomVariable(genInt); 
-  data.generateAct(genInt);
-  data.generateResp(genDouble); 
+  data.generateContVariable(genDouble, lDble, uDble);
+  data.generateOrdVariable(genInt, lInt, uInt);
+  data.generateNomVariable(genInt, lInt, uInt); 
+  data.generateAct(genInt, 0, 1);
+  data.generateResp(genDouble, lDble, uDble); 
 
   // Save the data into a csv file
   data.save(); 
