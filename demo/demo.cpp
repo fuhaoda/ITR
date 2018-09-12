fa9ba969f6992149bb487cd108d20f2ed078c9f5
Bo Zhang <zhang_bo3@lilly.com>
2018-09-12 09:13:09 -0400

    add a few options to the demo code

--- demo/demo.cpp
+++ demo/demo.cpp
@@ -1,11 +1,45 @@
 #include <string>
 #include <iostream>
+#include <getopt.h> 
 #include "ITR.h"
+
+void printUsage(char *program) {
+  fprintf(stdout, "Usage: %s [OPTIONS]\n"
+          "--data=STRING  Path to input file, default is sample100.csv\n"
+          "--thread=NUM   Number of threads to use, default is 1\n",
+          program);
+}
 
 int main(int argc, char **argv) {
   std::string ifile{"sample100.csv"};
-  unsigned nThreads = 1; 
+  unsigned nThreads = 1;
 
+  // Parse command line
+  static struct option long_options[] =
+    {
+     {"data", required_argument, 0, 'd'},
+     {"thread", required_argument, 0, 't'},
+     {"help", no_argument, 0, 'h'},
+     {0, 0, 0, 0}, 
+    };
+
+  int opt = 0;
+  int long_index = 0;
+  while ((opt = getopt_long(argc, argv, "d:t:h",
+                            long_options, &long_index)) != -1) {
+    switch (opt) {
+    case 'd':
+      ifile = optarg;
+      break;
+    case 't':
+      nThreads = atoi(optarg);
+      break;
+    case 'h':
+    case '?':
+      printUsage(argv[0]);
+      return -1;
+    }
+  }
   
   try {
     // Create an ITR instance. The constructor takes two arguments: The first
